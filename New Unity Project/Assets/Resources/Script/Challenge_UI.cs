36049bad2fd7fd7db180dfbeae6f3e27d5963043
DESKTOP-KUJLCQN\user <limnoung@hanmail.net>
2018-08-31 19:58:36 +0900

    758 합병

--- New Unity Project/Assets/Resources/Script/Challenge_UI.cs
+++ New Unity Project/Assets/Resources/Script/Challenge_UI.cs
@@ -4,44 +4,42 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Challenge_UI : MonoBehaviour
 {
-
-    [Header("상대 동아리 스테이터스")]
     private Stat[] gameInfo_stat;
     public Text[] status_Text;
+    public float divide = 2f; // 확률 계산용
     // Use this for initialization
     void Start()
     {
-        gameInfo_stat = GameInfo.Get_Data();
+        gameInfo_stat = Status.Get_Data();
         UIupdate();
         
     }
     void UIupdate()
     {
-        for(int i=0; i<4; i++)
+        float playerScore = gameInfo_stat[0].learning_Point * gameInfo_stat[0].participation / 100;
+        for (int i=1; i<5; i++)
         {
-            status_Text[i].text = "자금 : " + gameInfo_stat[i].fund + "인원 : " + gameInfo_stat[i].headCount +
-                "행복도 : " + gameInfo_stat[i].Happiness + "참여도 : " + gameInfo_stat[i].participation +
-                "학습도 : " + gameInfo_stat[i].learning_Point + "명성도 : " + gameInfo_stat[i].reputation;
+            if (gameInfo_stat[i].isEnabled) {
+                float enemyScore = gameInfo_stat[i].learning_Point * gameInfo_stat[i].participation / 100;
+                float score = playerScore - enemyScore / divide;
+                int sum = 0;
+                if (score > 0)
+                {
+                    for (int j = (int)score; j >= 1; j--)
+                        sum += j;
+                    if(sum / (playerScore / divide * enemyScore / divide) < 1f)
+                        status_Text[i - 1].text = "승리 확률 : " + sum / (playerScore / divide * enemyScore / divide)  * 100 + "%";
+                    else
+                        status_Text[i - 1].text = "승리 확률 : 100% !!! ";
+                }
+                else
+                {
+                    status_Text[i - 1].text = "좋지 않은 시도 입니다.";
+                }
+            }
+            else
+                status_Text[i - 1].text = "Unavailable";
         }
     }
-    void First()
-    {
-        
-    }
-    void Second()
-    {
 
-    }
-    void Third()
-    {
-
-    }
-    void Fourth()
-    {
-
-    }
-    void Back()
-    {
-        gameObject.SetActive(false);
-    }
 }
